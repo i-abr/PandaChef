d5c592794a1580a7073d8ce8fc404d853b0cf6dd
ian <iabr4073@gmail.com>
2020-11-05 14:42:52 -0500

    pan has weird effect, not actually deep so can't use. Consider updating model

--- panda_chef/panda_chef.py
+++ panda_chef/panda_chef.py
@@ -10,6 +10,18 @@ model = load_model_from_path(xml_path)
 sim = MjSim(model)
 viewer = MjViewer(sim)
 
+act_mid = np.mean(model.actuator_ctrlrange, axis=1)
+act_rng = 0.5 * (model.actuator_ctrlrange[:,1]-model.actuator_ctrlrange[:,0])
+
+sim.data.qpos[0] = act_mid[0]
+sim.data.qpos[1] = act_mid[1]
+sim.data.qpos[2] = act_mid[2]
+
+sim.data.ctrl[0] = act_mid[0]
+sim.data.ctrl[1] = act_mid[1]
+sim.data.ctrl[2] = act_mid[2]
+
+sim.forward()
 if __name__=='__main__':
 
     while True:
