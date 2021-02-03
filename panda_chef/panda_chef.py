6d245be1f5d2e012ff3bfe993c42a496a814c428
ian <iabr4073@gmail.com>
2021-02-03 17:22:54 -0500

    updated the panda model with a new pan, has annoying issues still

--- panda_chef/panda_chef.py
+++ panda_chef/panda_chef.py
@@ -4,7 +4,7 @@ from mujoco_py import load_model_from_path, MjSim, MjViewer
 
 
 
-xml_path = 'franka_panda.xml'
+xml_path = 'simple_flipper.xml'
 
 model = load_model_from_path(xml_path)
 sim = MjSim(model)
@@ -13,15 +13,7 @@ viewer = MjViewer(sim)
 act_mid = np.mean(model.actuator_ctrlrange, axis=1)
 act_rng = 0.5 * (model.actuator_ctrlrange[:,1]-model.actuator_ctrlrange[:,0])
 
-sim.data.qpos[0] = act_mid[0]
-sim.data.qpos[1] = act_mid[1]
-sim.data.qpos[2] = act_mid[2]
 
-sim.data.ctrl[0] = act_mid[0]
-sim.data.ctrl[1] = act_mid[1]
-sim.data.ctrl[2] = act_mid[2]
-
-sim.forward()
 if __name__=='__main__':
 
     while True:
